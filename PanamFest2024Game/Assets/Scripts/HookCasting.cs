f9126466c20daf9d73f4c96358257a99756f405c
kg718 <kggoodall18@gmail.com>
2024-01-16 15:36:34 +0000

    commit idk

--- PanamFest2024Game/Assets/Scripts/HookCasting.cs
+++ PanamFest2024Game/Assets/Scripts/HookCasting.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,7 +11,11 @@ public class HookCasting : MonoBehaviour
     [SerializeField] private Transform CastPoint;
     [SerializeField] private Transform HookPoint;
     [SerializeField] private GameObject BobberPrefab;
+    [HideInInspector] public BobberHooking Hooking;
 
+    [SerializeField] private CinemachineVirtualCamera VCam1;
+    [SerializeField] private CinemachineVirtualCamera VCam2;
+ 
     private GameObject Bobber;
 
     void Start()
@@ -18,6 +23,8 @@ public class HookCasting : MonoBehaviour
         Controls = new MasterInput();
         Controls.Enable();
         movement = GetComponent<PlayerMovement>();
+        VCam1.Priority = 1;
+        VCam2.Priority = 0;
     }
 
     void Update()
@@ -34,20 +41,32 @@ public class HookCasting : MonoBehaviour
 
     public void Cast()
     {
-        if(Bobber == null)
+        if (Bobber == null)
         {
             Bobber = Instantiate(BobberPrefab, CastPoint.position, Quaternion.identity);
             BobberLaunch Launcher = Bobber.GetComponent<BobberLaunch>();
             Launcher.TargetPosition = HookPoint.position;
+            VCam1.Priority = 0;
+            VCam2.Priority = 1;
         }
         else
         {
-            Destroy(Bobber);
+            if (!Hooking.HasHookedFish)
+            {
+                RetractLine();
+            }
         }
     }
 
     public void OnCastLine()
     {
         Cast();
+    }
+
+    public void RetractLine()
+    {
+        Destroy(Bobber);
+        VCam1.Priority = 1;
+        VCam2.Priority = 0;
     }
 }
